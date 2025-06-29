4b411b7ac82b7ae40d77cf722cdc56b6d1e86181
Rita Castro <ritamcastro@gmail.com>
2025-06-29 19:09:41 +0100

    ♻️ Parameterize the test for rule #1

--- engine/src/moves.test.ts
+++ engine/src/moves.test.ts
@@ -1,72 +1,64 @@
 import { describe, expect, it } from "vitest";
 import { Board } from "./board";
+import type { Direction } from "./model";
 
 describe("Rule #1: Numbers move as far as possible in the pushing direction", () => {
-	it("pushes the tiles RIGHT when can't merge them", () => {
-		const game = new Board([
-			[0, 2, 0, 5],
-			[0, 21, 0, 55],
-		]);
+	const boardHorizontal = [
+		[0, 2, 0, 5],
+		[0, 21, 0, 55],
+	];
+	const boardVertical = [
+		[2, 1],
+		[0, 3],
+		[5, 8],
+		[0, 21],
+	];
+	it.each([
+		{
+			board: boardHorizontal,
+			direction: "RIGHT",
+			expectedBoard: [
+				[0, 0, 2, 5],
+				[0, 0, 21, 55],
+			],
+		},
+		{
+			board: boardHorizontal,
+			direction: "LEFT",
+			expectedBoard: [
+				[2, 5, 0, 0],
+				[21, 55, 0, 0],
+			],
+		},
+		{
+			board: boardVertical,
+			direction: "UP",
+			expectedBoard: [
+				[2, 1],
+				[5, 3],
+				[0, 8],
+				[0, 21],
+			],
+		},
+		{
+			board: boardVertical,
+			direction: "DOWN",
+			expectedBoard: [
+				[0, 1],
+				[0, 3],
+				[2, 8],
+				[5, 21],
+			],
+		},
+	])(
+		"pushes the tiles $direction when can't merge them",
+		({ board, direction, expectedBoard }) => {
+			const game = new Board(board);
 
-		game.push("RIGHT");
+			game.push(direction as Direction);
 
-		const finalState = game.getState();
-		expect(finalState).toEqual([
-			[0, 0, 2, 5],
-			[0, 0, 21, 55],
-		]);
-	});
-
-	it("pushes the tiles LEFT when can't merge them", () => {
-		const game = new Board([
-			[0, 2, 0, 5],
-			[0, 21, 0, 55],
-		]);
-
-		game.push("LEFT");
-
-		const finalState = game.getState();
-		expect(finalState).toEqual([
-			[2, 5, 0, 0],
-			[21, 55, 0, 0],
-		]);
-	});
-
-	it("pushes the tiles UP when can't merge them", () => {
-		const game = new Board([
-			[2, 1],
-			[0, 3],
-			[5, 8],
-			[0, 21],
-		]);
-
-		game.push("UP");
-
-		const finalState = game.getState();
-		expect(finalState).toEqual([
-			[2, 1],
-			[5, 3],
-			[0, 8],
-			[0, 21],
-		]);
-	});
-
-	it("pushes the tiles DOWN when can't merge them", () => {
-		const game = new Board([
-			[2, 1],
-			[0, 3],
-			[5, 8],
-			[0, 21],
-		]);
-
-		game.push("DOWN");
-
-		const finalState = game.getState();
-		expect(finalState).toEqual([
-			[0, 1],
-			[0, 3],
-			[2, 8],
-			[5, 21],
-		]);
-	});
+			const finalState = game.getState();
+			expect(finalState).toEqual(expectedBoard);
+		},
+	);
 });
