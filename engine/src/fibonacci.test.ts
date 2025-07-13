227655833f95cbd5e97e63bbcfcb2130a229d650
Rita Castro <ritamcastro@gmail.com>
2025-07-13 13:57:15 +0200

    ✅ Update Fibonacci sequence to cover the edge cases

---
+++ engine/src/fibonacci.test.ts
@@ -0,0 +1,94 @@
+import { describe, expect, it } from "vitest";
+import { areConsecutiveInFibSequence } from "./fibonacci";
+
+// value: 0 1 1 2 3 5 8
+// index: 0 1 2 3 4 5 6
+
+describe("The Fibonacci Sequence...", () => {
+	it.each([
+		{
+			n1: 5,
+			n2: 8,
+			areConsecutive: true,
+		},
+		{
+			n1: 3,
+			n2: 5,
+			areConsecutive: true,
+		},
+
+		{
+			n1: 21,
+			n2: 34,
+			areConsecutive: true,
+		},
+	])(
+		"can evaluate if entries are consecutive in ascending order",
+		({ n1, n2, areConsecutive }) => {
+			const consecutive = areConsecutiveInFibSequence(n1, n2);
+
+			expect(consecutive).toBe(areConsecutive);
+		},
+	);
+
+	it.each([
+		{
+			n2: 5,
+			n1: 8,
+			areConsecutive: true,
+		},
+		{
+			n2: 3,
+			n1: 5,
+			areConsecutive: true,
+		},
+
+		{
+			n2: 21,
+			n1: 34,
+			areConsecutive: true,
+		},
+	])(
+		"can evaluate if entries are consecutive in descending order",
+		({ n1, n2, areConsecutive }) => {
+			const consecutive = areConsecutiveInFibSequence(n1, n2);
+
+			expect(consecutive).toBe(areConsecutive);
+		},
+	);
+
+	it.each([
+		{
+			n2: 0,
+			n1: 1,
+			areConsecutive: true,
+		},
+		{
+			n2: 1,
+			n1: 0,
+			areConsecutive: true,
+		},
+		{
+			n2: 1,
+			n1: 1,
+			areConsecutive: true,
+		},
+		{
+			n2: 1,
+			n1: 2,
+			areConsecutive: true,
+		},
+		{
+			n2: 2,
+			n1: 1,
+			areConsecutive: true,
+		},
+	])(
+		"can evaluate the egde cases at the start of the sequence: ($n1 -> $n2)",
+		({ n1, n2, areConsecutive }) => {
+			const consecutive = areConsecutiveInFibSequence(n1, n2);
+
+			expect(consecutive).toBe(areConsecutive);
+		},
+	);
+});
--- engine/src/fibonacci.ts
+++ engine/src/fibonacci.ts
@@ -1,26 +1,25 @@
 const generateFibonacci = (limit: number): number[] => {
-    const fib = new Array<number>(limit)
-    fib[0] = 0
-    fib[1] = 1
-    
-    for (let index = 2; index < limit; index++) {
-        fib[index] = fib[index - 1] + fib[index - 2]
-    }
-    
-    return fib
-}
+	const fib = new Array<number>(limit);
+	fib[0] = 0;
+	fib[1] = 1;
 
-const fibSequence = generateFibonacci(2^16)
+	for (let index = 2; index < limit; index++) {
+		fib[index] = fib[index - 1] + fib[index - 2];
+	}
 
-const areConsecutiveInFibSequence = (
-	n1: number,
-	n2: number,
-): boolean => {
-	const index2 = fibSequence.indexOf(n1);
-	const index1 = fibSequence.indexOf(n2);
-
-	return index1 === index2 - 1;
+	return fib;
 };
 
+const fibSequence = generateFibonacci(2 ^ 16);
 
-export {areConsecutiveInFibSequence}
+const areConsecutiveInFibSequence = (n1: number, n2: number): boolean => {
+	if (n1 === n2 && n1 === 1) return true;
+	if (n1 === 2 || n2 === 2 && n1 === 1 || n2 === 1) return true;
+	
+	const index1 = fibSequence.indexOf(n1);
+	const index2 = fibSequence.indexOf(n2);
+
+	return index1 === index2 - 1 || index2 === index1 - 1;
+};
+
+export { areConsecutiveInFibSequence };
