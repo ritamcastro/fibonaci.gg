7f0aa411c0885cca4ee9baad20f819d996f5493b
Rita Castro <ritamcastro@gmail.com>
2026-02-06 18:07:53 +0000

    🗑️ Deleted the engine

--- engine/src/board.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from "vitest";
-import { Board } from "./board";
-
-describe("The Board for Fibonacci", () => {
-	it("thows an error if the board that is not squared", () => {
-		expect(() => new Board([[1, 2]])).toThrowError("The Board needs to be squared!")
-	})
-})
--- engine/src/model.ts
+++
@@ -1,4 +0,0 @@
-const Directions = ['UP', "DOWN", "LEFT", "RIGHT"] as const
-type Direction = typeof Directions[number]
-
-export type {Direction}
