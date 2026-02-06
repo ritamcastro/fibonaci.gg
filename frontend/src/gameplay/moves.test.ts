951fb092696b2bf23b31d0ec35749686dede7670
Rita Castro <ritamcastro@gmail.com>
2026-02-06 17:14:57 +0000

    👔 Add the remaining rules

--- frontend/src/gameplay/moves.test.ts
+++ frontend/src/gameplay/moves.test.ts
@@ -1,18 +1,368 @@
+// describe("Moving the tiles", () => {
+// 	it.each`
+// 		direction  | initialBoard                                                | expectedBoard
+// 		${"RIGHT"} | ${[[0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 0]]} | ${[[0, 0, 0, 0], [0, 0, 0, 1], [0, 0, 0, 1], [0, 0, 0, 0]]}
+// 		${"LEFT"}  | ${[[0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 0]]} | ${[[0, 0, 0, 0], [1, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0]]}
+// 		${"UP"}    | ${[[0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 0]]} | ${[[0, 1, 1, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]}
+// 		${"DOWN"}  | ${[[0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 0]]} | ${[[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 1, 1, 0]]}
+// 	`(
+// 		"moves the board $direction",
+// 		({ initialBoard, direction, expectedBoard }) => {
+// 			const moved = applyMove(initialBoard, direction);
+
+import type { Direction } from "../constants";
+import { getRandomPosition } from "../utils/math";
 import { applyMove } from "./moves";
 
-describe("Moving the tiles", () => {
-	it.each`
-		direction  | initialBoard                                                | expectedBoard
-		${"RIGHT"} | ${[[0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 0]]} | ${[[0, 0, 0, 0], [0, 0, 0, 1], [0, 0, 0, 1], [0, 0, 0, 0]]}
-		${"LEFT"}  | ${[[0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 0]]} | ${[[0, 0, 0, 0], [1, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0]]}
-		${"UP"}    | ${[[0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 0]]} | ${[[0, 1, 1, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]}
-		${"DOWN"}  | ${[[0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 0]]} | ${[[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 1, 1, 0]]}
-	`(
-		"moves the board $direction",
-		({ initialBoard, direction, expectedBoard }) => {
-			const moved = applyMove(initialBoard, direction);
+vi.mock("../utils/math", () => ({
+	getRandomPosition: vi.fn(),
+}));
+
+describe("Rule set!", () => {
+	describe("Rule #1: Numbers move as far as possible in the pushing direction", () => {
+		const boardHorizontal = [
+			[0, 2, 0, 5],
+			[0, 21, 0, 55],
+			[0, 0, 0, 0],
+			[0, 0, 0, 0],
+		];
+		const boardVertical = [
+			[2, 1, 0, 0],
+			[0, 3, 0, 0],
+			[5, 8, 0, 0],
+			[0, 21, 0, 0],
+		];
+		it.each([
+			{
+				board: boardHorizontal,
+				direction: "RIGHT",
+				expectedBoard: [
+					[0, 0, 2, 5],
+					[0, 0, 21, 55],
+					[0, 0, 0, 0],
+					[0, 0, 0, 0],
+				],
+			},
+			{
+				board: boardHorizontal,
+				direction: "LEFT",
+				expectedBoard: [
+					[2, 5, 0, 0],
+					[21, 55, 0, 0],
+					[0, 0, 0, 0],
+					[0, 0, 0, 0],
+				],
+			},
+			{
+				board: boardVertical,
+				direction: "UP",
+				expectedBoard: [
+					[2, 1, 0, 0],
+					[5, 3, 0, 0],
+					[0, 8, 0, 0],
+					[0, 21, 0, 0],
+				],
+			},
+			{
+				board: boardVertical,
+				direction: "DOWN",
+				expectedBoard: [
+					[0, 1, 0, 0],
+					[0, 3, 0, 0],
+					[2, 8, 0, 0],
+					[5, 21, 0, 0],
+				],
+			},
+		])(
+			"pushes the tiles $direction when can't merge them",
+			({ board, direction, expectedBoard }) => {
+				const moved = applyMove(board, direction as Direction);
+
+				expect(moved).toEqual(expectedBoard);
+			},
+		);
+	});
+
+	describe("Rule #2: When two consecutive numbers in the Fibonacci sequence are moveed one on another, they fuse into the next number.", () => {
+		const boardHorizontal = [
+			[0, 2, 0, 3],
+			[0, 21, 0, 55],
+			[0, 0, 0, 0],
+			[0, 0, 0, 0],
+		];
+
+		const boardVertical = [
+			[2, 1, 0, 0],
+			[0, 3, 0, 0],
+			[3, 8, 0, 0],
+			[0, 21, 0, 0],
+		];
+
+		it.each([
+			{
+				board: boardHorizontal,
+				direction: "RIGHT",
+				expectedBoard: [
+					[0, 0, 0, 5],
+					[0, 0, 21, 55],
+					[0, 0, 0, 0],
+					[0, 0, 0, 0],
+				],
+			},
+			{
+				board: boardHorizontal,
+				direction: "LEFT",
+				expectedBoard: [
+					[5, 0, 0, 0],
+					[21, 55, 0, 0],
+					[0, 0, 0, 0],
+					[0, 0, 0, 0],
+				],
+			},
+			{
+				board: boardVertical,
+				direction: "UP",
+				expectedBoard: [
+					[5, 1, 0, 0],
+					[0, 3, 0, 0],
+					[0, 8, 0, 0],
+					[0, 21, 0, 0],
+				],
+			},
+			{
+				board: boardVertical,
+				direction: "DOWN",
+				expectedBoard: [
+					[0, 1, 0, 0],
+					[0, 3, 0, 0],
+					[0, 8, 0, 0],
+					[5, 21, 0, 0],
+				],
+			},
+		])(
+			"pushes the tiles $direction and merge them",
+			({ board, direction, expectedBoard }) => {
+				const moved = applyMove(board, direction as Direction);
+
+				expect(moved).toEqual(expectedBoard);
+			},
+		);
+	});
+
+	describe("Rule #3.1: Fusing orders are resolved in the backward direction of the push.", () => {
+		it.each([
+			{
+				board: [
+					[0, 1, 2, 3],
+					[0, 3, 2, 1],
+					[0, 0, 0, 0],
+					[0, 0, 0, 0],
+				],
+				direction: "RIGHT",
+				expectedBoard: [
+					[0, 0, 1, 5],
+					[0, 0, 3, 3],
+					[0, 0, 0, 0],
+					[0, 0, 0, 0],
+				],
+			},
+			{
+				board: [
+					[0, 3, 2, 1],
+					[0, 5, 3, 5],
+					[0, 0, 0, 0],
+					[0, 0, 0, 0],
+				],
+				direction: "LEFT",
+				expectedBoard: [
+					[3, 3, 0, 0],
+					[5, 8, 0, 0],
+					[0, 0, 0, 0],
+					[0, 0, 0, 0],
+				],
+			},
+			{
+				board: [
+					[0, 0, 0, 0],
+					[3, 5, 0, 0],
+					[2, 3, 0, 0],
+					[1, 5, 0, 0],
+				],
+				direction: "UP",
+				expectedBoard: [
+					[3, 5, 0, 0],
+					[3, 8, 0, 0],
+					[0, 0, 0, 0],
+					[0, 0, 0, 0],
+				],
+			},
+			{
+				board: [
+					[0, 0, 0, 0],
+					[3, 5, 0, 0],
+					[2, 3, 0, 0],
+					[1, 5, 0, 0],
+				],
+				direction: "DOWN",
+				expectedBoard: [
+					[0, 0, 0, 0],
+					[0, 0, 0, 0],
+					[3, 5, 0, 0],
+					[3, 8, 0, 0],
+				],
+			},
+		])(
+			"moves the board $direction and merges the tiles",
+			({ board, direction, expectedBoard }) => {
+				const moved = applyMove(board, direction as Direction);
+
+				expect(moved).toEqual(expectedBoard);
+			},
+		);
+	});
+
+	describe("Rule #3.2 A fused number can not be fused once again in the same turn.", () => {
+		it("moves the board", () => {
+			const board = [
+				[1, 2, 3, 5],
+				[0, 3, 2, 1],
+				[0, 0, 0, 0],
+				[0, 0, 0, 0],
+			];
+			const direction = "RIGHT";
+			const expectedBoard = [
+				[0, 0, 3, 8],
+				[0, 0, 3, 3],
+				[0, 0, 0, 0],
+				[0, 0, 0, 0],
+			];
+
+			const moved = applyMove(board, direction as Direction);
 
 			expect(moved).toEqual(expectedBoard);
-		},
-	);
+		});
+	});
+
+	describe("Rule #4: Numbers can move to a square that a fusing has just emptied.", () => {
+		it.each([
+			{
+				board: [
+					[1, 1, 1, 1],
+					[1, 0, 3, 5],
+					[0, 0, 0, 0],
+					[0, 0, 0, 0],
+				],
+				direction: "RIGHT",
+				expectedBoard: [
+					[0, 0, 2, 2],
+					[0, 0, 1, 8],
+					[0, 0, 0, 0],
+					[0, 0, 0, 0],
+				],
+			},
+			{
+				board: [
+					[1, 1, 1, 1],
+					[1, 0, 3, 5],
+					[0, 0, 0, 0],
+					[0, 0, 0, 0],
+				],
+				direction: "LEFT",
+				expectedBoard: [
+					[2, 2, 0, 0],
+					[1, 8, 0, 0],
+					[0, 0, 0, 0],
+					[0, 0, 0, 0],
+				],
+			},
+			{
+				board: [
+					[1, 0, 0, 0],
+					[1, 0, 0, 0],
+					[2, 0, 0, 0],
+					[3, 0, 0, 0],
+				],
+				direction: "UP",
+				expectedBoard: [
+					[2, 0, 0, 0],
+					[5, 0, 0, 0],
+					[0, 0, 0, 0],
+					[0, 0, 0, 0],
+				],
+			},
+			{
+				board: [
+					[1, 0, 0, 0],
+					[1, 0, 0, 0],
+					[2, 0, 0, 0],
+					[3, 0, 0, 0],
+				],
+				direction: "DOWN",
+				expectedBoard: [
+					[0, 0, 0, 0],
+					[0, 0, 0, 0],
+					[2, 0, 0, 0],
+					[5, 0, 0, 0],
+				],
+			},
+		])(
+			"moves the board $direction and merges the tiles",
+			({ board, direction, expectedBoard }) => {
+				const moved = applyMove(board, direction as Direction);
+
+				expect(moved).toEqual(expectedBoard);
+			},
+		);
+	});
+
+	describe("Rule #5: New tiles are added", () => {
+		it("Adds tile with the start of the sequence when at least two tiles are merged", () => {
+			vi.mocked(getRandomPosition)
+				.mockReturnValueOnce(2)
+				.mockReturnValueOnce(1);
+
+			const boardHorizontal = [
+				[0, 2, 0, 5],
+				[0, 21, 0, 55],
+				[0, 0, 0, 0],
+				[0, 0, 0, 0],
+			];
+			const direction = "RIGHT";
+			const expectedBoard = [
+				[0, 0, 2, 5],
+				[0, 0, 21, 55],
+				[0, 1, 0, 0],
+				[0, 0, 0, 0],
+			];
+
+			const moved = applyMove(boardHorizontal, direction as Direction);
+
+			expect(moved).toEqual(expectedBoard);
+		});
+
+		it("Adds tile only to available spaces", () => {
+			vi.mocked(getRandomPosition)
+				.mockReturnValueOnce(2)
+				.mockReturnValueOnce(1);
+
+			const boardHorizontal = [
+				[21, 21, 21, 21],
+				[55, 0, 55, 55],
+				[144, 144, 144, 144],
+				[377, 377, 377, 377],
+			];
+
+			const direction = "RIGHT";
+			const expectedBoard = [
+				[21, 21, 21, 21],
+				[1, 55, 55, 55],
+				[144, 144, 144, 144],
+				[377, 377, 377, 377],
+			];
+
+			const moved = applyMove(boardHorizontal, direction as Direction);
+
+			expect(moved).toEqual(expectedBoard);
+		});
+	});
 });
--- frontend/src/gameplay/moves.ts
+++ frontend/src/gameplay/moves.ts
@@ -1,25 +1,69 @@
 import type { Direction } from "../constants";
+import { areConsecutiveInFibSequence } from "../utils/fibonacci";
+
+const mergeTiles = (subline: number[]): number[] => {
+	const lineSize = subline.length;
+	const reversedLine = [...subline].reverse();
+
+	const merged: number[] = new Array(lineSize).fill(0);
+	const isFused: boolean[] = new Array(lineSize).fill(false);
+
+	for (let i = 0; i < lineSize; i++) {
+		if (!isFused[i] && !isFused[i + 1]) {
+			if (areConsecutiveInFibSequence(reversedLine[i], reversedLine[i + 1])) {
+				merged[i] = reversedLine[i] + reversedLine[i + 1];
+				isFused[i] = true;
+				isFused[i + 1] = true;
+			} else {
+				merged[i] = reversedLine[i];
+			}
+		}
+	}
+
+	return merged.filter((n) => n !== 0).reverse();
+};
+
+type MovedTiles = {
+	movedTiles: number[];
+	hasMerged: boolean;
+};
 
 /**
  * Moves tiles to the left by padding with zeros on the right
  * Example: [0, 1, 0, 2] -> [1, 2, 0, 0]
  */
-const leftPadWithZeros = (line: number[]): number[] => {
+const leftPadWithZeros = (line: number[]): MovedTiles => {
 	const zeros = line.filter((n) => n === 0);
 	const nonZeros = line.filter((n) => n !== 0);
 
-	return [...zeros, ...nonZeros];
+	const merged = mergeTiles(nonZeros);
+	const whatToPad = nonZeros.length - merged.length;
+
+	const movedTiles =
+		merged.length === 0
+			? [...zeros, ...nonZeros]
+			: [...zeros, ...new Array(whatToPad).fill(0), ...merged];
+
+	return { movedTiles, hasMerged: merged.length !== 0 };
 };
 
 /**
  * Moves tiles to the right by padding with zeros on the left
  * Example: [0, 1, 0, 2] -> [0, 0, 1, 2]
  */
-const rightPadWithZeros = (line: number[]): number[] => {
+const rightPadWithZeros = (line: number[]): MovedTiles => {
 	const zeros = line.filter((n) => n === 0);
 	const nonZeros = line.filter((n) => n !== 0);
 
-	return [...nonZeros, ...zeros];
+	const merged = mergeTiles(nonZeros);
+	const whatToPad = nonZeros.length - merged.length;
+
+	const movedTiles =
+		merged.length === 0
+			? [...nonZeros, ...zeros]
+			: [...merged, ...new Array(whatToPad).fill(0), ...zeros];
+
+	return { movedTiles, hasMerged: merged.length !== 0 };
 };
 
 const applyMove = (board: number[][], direction: Direction) => {
@@ -27,14 +71,20 @@ const applyMove = (board: number[][], direction: Direction) => {
 	const ncols = board[0].length;
 	const newBoard = board.map((row) => [...row]); // Deep copy
 
+	let merged = false;
+
 	if (direction === "RIGHT" || direction === "LEFT") {
 		// Process each row
 		for (let row = 0; row < nrows; row++) {
-			const movedRow =
+			const { movedTiles, hasMerged } =
 				direction === "RIGHT"
 					? leftPadWithZeros(newBoard[row])
 					: rightPadWithZeros(newBoard[row]);
-			newBoard[row] = movedRow;
+			newBoard[row] = movedTiles;
+
+			if (hasMerged) {
+				merged = true;
+			}
 		}
 	}
 	if (direction === "UP" || direction === "DOWN") {
@@ -45,18 +95,44 @@ const applyMove = (board: number[][], direction: Direction) => {
 				column.push(newBoard[row][col]);
 			}
 
-			const movedCol =
+			const { movedTiles, hasMerged } =
 				direction === "UP"
 					? rightPadWithZeros(column)
 					: leftPadWithZeros(column);
 
 			for (let row = 0; row < nrows; row++) {
-				newBoard[row][col] = movedCol[row];
+				newBoard[row][col] = movedTiles[row];
+			}
+
+			if (hasMerged) {
+				merged = true;
 			}
 		}
 	}
 
+	if (merged) {
+		const emptyTile = getPositionForEmptyTile(newBoard);
+
+		newBoard[emptyTile[0]][emptyTile[1]] = 1;
+	}
+
 	return newBoard;
+};
+
+const getPositionForEmptyTile = (board: number[][]) => {
+	const emptyPositions: number[][] = [];
+
+	for (let row = 0; row < board.length; row++) {
+		for (let col = 0; col < board[row].length; col++) {
+			if (board[row][col] === 0) {
+				const pos = [row, col];
+				emptyPositions.push(pos);
+			}
+		}
+	}
+
+	const index = Math.ceil(Math.random() * emptyPositions.length) - 1;
+	return emptyPositions[index];
 };
 
 export { applyMove };
