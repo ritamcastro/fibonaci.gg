05aba59efe2b65f1fe53fc4646f8d5f11fade637
Rita Castro <ritamcastro@gmail.com>
2026-02-07 23:02:52 +0000

    🍱 Add the logo and the favicon that is the spiral

--- frontend/src/ui/templates/layout.tsx
+++ frontend/src/ui/templates/layout.tsx
@@ -1,19 +1,19 @@
 import { Outlet } from "react-router";
 import type { ReactNode } from "react";
+import logo from "../../assets/logo.svg";
 
 type LayoutProps = {
 	children?: ReactNode;
 };
-
+console.log(logo);
 const Layout = ({ children }: LayoutProps) => {
 	return (
 		<>
 			<header>
-				<h1>Fibonacci meets 2048</h1>
+				{/* <h1>Fibonacci meets 2024</h1> */}
+				<img src={logo} aria-label="Fibonacci meets 2048" />
 			</header>
-			<main id="main">
-				{children ?? <Outlet />}
-			</main>
+			<main id="main">{children ?? <Outlet />}</main>
 			<footer>©️ ritamcastro 2026</footer>
 		</>
 	);
